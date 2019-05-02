e844e39638f168f0c62bbd0c5f62ed29a983b876
Sean McIlvenna <sean.mcilvenna@lantanagroup.com>
2019-05-02 09:22:22 -0700

    Adding logging to main server starterup

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,13 +1,16 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app/app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app/app.module';
+import {IServerConfig} from './app/server-config';
+import {Logger} from '@nestjs/common';
+
 import * as bodyParser from 'body-parser';
 import * as path from 'path';
 import * as config from 'config';
-import { IServerConfig } from './app/server-config';
 
 const serverConfig = <IServerConfig> config.get('server');
 
 async function bootstrap() {
+  const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix(`api`);
 
@@ -17,7 +20,8 @@ async function bootstrap() {
 
   const port = process.env.port || serverConfig.port || 3333;
   await app.listen(port, () => {
-    console.log(`Listening at http://localhost:${port}`);
+    logger.log(`Listening at http://localhost:${port}`);
+    logger.log(`Server configured to use FHIR server ${serverConfig.fhirServerBase}`);
   });
 }
 
