55f7405adefbce0655cf18bcf6c478df48bafa17
mbarr <mbarr@ruvos.com>
2020-03-26 15:12:33 -0400

    moved download button to Home component

--- apps/client/src/app/app.component.ts
+++ apps/client/src/app/app.component.ts
@@ -8,26 +8,9 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  constructor(
-    private httpClient: HttpClient,
-    private authService: AuthService
-  ) {}
+  constructor(public authService: AuthService) {}
 
   ngOnInit(): void {
     this.authService.checkSession();
-  }
-
-  request: any = {};
-
-  download() {
-    this.httpClient
-      .post('/api/download', this.request)
-      .toPromise()
-      .then(() => {
-        console.log('successfully sent download post request');
-      })
-      .catch(err => {
-        console.log(err);
-      });
   }
 }
--- apps/client/src/app/home/home.component.ts
+++ apps/client/src/app/home/home.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  constructor(
+    private httpClient: HttpClient,
+    private authService: AuthService
+  ) {}
 
-  constructor(public authService: AuthService) { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  request: any = {};
+
+  download() {
+    this.httpClient
+      .post('/api/download', this.request)
+      .toPromise()
+      .then(() => {
+        console.log('successfully sent download post request');
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
-
 }
