91de27d33fa0ba53259c30fa68eb9c8d85b03a3c
Junaid Siddiqui <junaid.wim@gmail.com>
2022-07-06 10:26:44 -0400

    wip fieldsets

--- apps/server/src/app/app.module.ts
+++ apps/server/src/app/app.module.ts
@@ -4,13 +4,14 @@ import { AppService } from './app.service';
 import { UserController } from './user/user.controller';
 import { FhirController } from './fhir/fhir.controller';
 import { AuthModule } from './auth-module/auth.module';
+import { FormsModule } from '@angular/forms';
 import { SubscriptionController } from './subscription/subscription.controller';
 import { ApiKeysController } from './api-key/api-keys.controller';
 import { UploadController } from './upload/upload.controller';
 import { DownloadController } from './download/download.controller';
 
 @Module({
-  imports: [AuthModule, HttpModule],
+  imports: [AuthModule, HttpModule, FormsModule],
   controllers: [
     AppController,
     UserController,
