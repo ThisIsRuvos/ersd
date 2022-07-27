d5461feffc0a74a16a118bf8e6c5d51a23dcc3c7
Junaid Siddiqui <junaid.wim@gmail.com>
2022-07-27 12:24:20 -0400

    controller for returning xml and json

---
+++ apps/server/src/app/s3/s3.controller.ts
@@ -0,0 +1,112 @@
+import { 
+  Controller,
+  Header,
+  HttpService,
+  Post,
+  Logger,
+  UseGuards,
+  Query,
+  Get,
+} from '@nestjs/common';
+import { AppService } from '../app.service';
+import { AuthGuard } from '@nestjs/passport';
+import S3 from 'aws-sdk/clients/s3';
+
+@Controller('s3')
+export class S3Controller {
+  private readonly logger = new Logger('S3Controller');
+  constructor(private httpService: HttpService, private appService: AppService) {
+  }
+
+  private setJSONKey(version, bundle?) {
+    let key = ''
+    if(version == 'ecrv1') {
+      key = this.appService.serverConfig.payload.ERSDV1_JSON_KEY
+    } else if (version == 'ecrv2' && bundle !== '') {
+      switch(bundle) {
+        case 'supplemental':
+          key = this.appService.serverConfig.payload.ERSDV2_SUPPLEMENTAL_JSON_KEY
+        case 'specification':
+          key = this.appService.serverConfig.payload.ERSDV2_SPECIFICATION_JSON_KEY
+      }
+    }
+    return key
+  }
+
+  private setXMLKey(version, bundle?) {
+    let key = ''
+    if(version == 'ecrv1') {
+      key = this.appService.serverConfig.payload.ERSDV1_XML_KEY
+    } else if (version == 'ecrv2' && bundle !== '') {
+      switch(bundle) {
+        case 'supplemental':
+          key = this.appService.serverConfig.payload.ERSDV2_SUPPLEMENTAL_XML_KEY
+        case 'specification':
+          key = this.appService.serverConfig.payload.ERSDV2_SPECIFICATION_XML_KEY
+      }
+    }
+    return key
+  }
+
+  @Post('json')
+  @UseGuards(AuthGuard())
+  @Header('Content-Type', 'application/json')
+  @Header('Content-Disposition', 'attachment; filename=bundle.json')
+  async getJSON(@Query() queryParams) {
+    const Bucket = this.appService.serverConfig.payload.Bucket;
+    const Key = this.setJSONKey(queryParams.version, queryParams.bundle)
+
+    if (typeof Bucket === 'undefined' || Bucket === '' || Key === '') {
+      const errorMessage = 'Failed to download from S3, no Bucket or Key specified'
+      this.logger.error(errorMessage);
+      throw Error(errorMessage);
+    } 
+
+    const s3client = new S3({ 
+      s3ForcePathStyle: true,
+      accessKeyId: 'S3RVER',
+      secretAccessKey: 'S3RVER',
+      endpoint: 'http://localhost:4568'
+    });
+
+    const headParams = {
+      Bucket,
+      Key,
+    }
+
+    const data = await s3client.getObject(headParams).promise();
+    const parsedJSON = JSON.parse(data.Body.toString('utf-8'))
+    return parsedJSON
+  }
+
+  @Get('xml')
+  @UseGuards(AuthGuard())
+  @Header('Content-Type', 'text/xml')
+  async getXML(@Query() queryParams) {
+    const Bucket = this.appService.serverConfig.payload.Bucket;
+    const Key = this.setXMLKey(queryParams.version, queryParams.bundle)
+
+    if (typeof Bucket === 'undefined' || Bucket === '' || Key === '') {
+      const errorMessage = 'Failed to download from S3, no Bucket or Key specified'
+      this.logger.error(errorMessage);
+      throw Error(errorMessage);
+    } 
+
+    const s3client = new S3({ 
+      s3ForcePathStyle: true,
+      accessKeyId: 'S3RVER',
+      secretAccessKey: 'S3RVER',
+      endpoint: 'http://localhost:4568'
+    });
+
+    const headParams = {
+      Bucket,
+      Key,
+    }
+
+    const data = await s3client.getObject(headParams).promise();
+    const body = data.Body.toString('utf-8')
+    return body
+  }
+}
+
