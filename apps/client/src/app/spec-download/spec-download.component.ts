40a9e1d31a6c3728d396c30a46c00db726756abb
Junaid Siddiqui <junaid.wim@gmail.com>
2022-07-27 12:25:36 -0400

    client changes to request JSON/XML and save as file

--- apps/client/src/app/spec-download/spec-download.component.ts
+++ apps/client/src/app/spec-download/spec-download.component.ts
@@ -2,10 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import saveAs from 'save-as';
 
-interface PayloadDownload {
-  url: string;
-}
-
 @Component({
   selector: 'ersd-spec-download',
   templateUrl: './spec-download.component.html',
@@ -13,26 +9,8 @@ interface PayloadDownload {
 })
 export class SpecDownloadComponent implements OnInit {
   request: any = {};
-  // THOUGHTS: we might just be able to dynamically generate the URL based on input
-  // rather than searching through an object for URLs
-  // eRSDVersions: Record<string, any> = {
-  //   version_one: {
-  //     xml: "",
-  //     json: ""
-  //   },
-  //   version_two: {
-  //     specification_bundle: {
-  //       xml: "",
-  //       json: ""
-  //     } ,
-  //     supplemental_bundle: {
-  //       xml: "",
-  //       json: ""
-  //     }
-  //   }
-  // };
 
-  version = "ecr-v1"
+  version = "ecrv1"
   bundleType = ""
   contentType = "json"
 
@@ -49,80 +27,65 @@ export class SpecDownloadComponent implements OnInit {
 
   setContentType(e) { this.contentType = e.target.value }
 
-  async handleSubmit() {
-    try {
-      if (this.version == "ecr-v2" && this.bundleType == "") { 
-        throw Error('Please select a content type')
-      }
+  buildFileName() {
+    if (this.bundleType !== "") {
+      return `${this.version}-${this.bundleType}.${this.contentType}`
+    }
+    return `${this.version}.${this.contentType}`
+  }
 
-      this.downloadS3('http://localhost:4568/test-bucket/bundle.json')
+  async handleSubmit() {
+    let url = ""
+    try {
+      if (this.version == "ecrv2") {
+        if(this.bundleType == "") { throw Error('Please select a bundle type') } 
+        url = `/api/s3/${this.contentType}?version=${this.version}&bundle=${this.bundleType}`
+      } else {
+        url = `/api/s3/${this.contentType}?version=${this.version}`
+      }
+      if (this.contentType === 'json') {
+        return this.queryServer(url)
+      } else {
+        return this.queryServerXML(url)
+      }
     } catch (err) {
       alert(`Error while downloading file: ${err.message}`);
       console.error(err);
     }
   }
 
-  async downloadFile(data, filename) {
-    const url = data.url
-    console.log('Downloading');
-    // if (url.includes('local')) {
-    //   return await this.downloadLocal(url, filename)
-    // }
-    // else {
-      return await this.downloadS3(url)
-    // }
+  async queryServer(url) {
+    this.httpClient
+      .post(url, this.request)
+      .toPromise()
+      .then(async (data) => {
+        await this.downloadFile(data, this.buildFileName())
+      })
+      .catch(err => {
+        console.error(err);
+      });
   }
-  async downloadLocal(url, filename) {
+
+  async queryServerXML(url) {
     try {
-      const results = await this.httpClient.get(url, { responseType: 'blob' }).toPromise();
-      saveAs(results, filename);
-    } catch (ex) {
-      alert(`Error while downloading file: ${ex.message}`);
-      console.error(ex);
+      this.httpClient
+        .get(url, { responseType: 'text' })
+        .subscribe(async result => {
+          await this.downloadFile(result, this.buildFileName())
+        })
+    } catch (err) {
+      alert(`Error downloading file: ${err.message}`)
+      console.error(err)
     }
   }
-  async downloadS3(url) {
-    var a = document.createElement('a');
-    a.href = url;
-    a.style.display = 'none';
-    document.body.appendChild(a);
-    a.click();
-    a.parentNode.removeChild(a);
-  }
 
-  async downloadExcel() {
-    this.httpClient
-      .post('/api/download/excel', this.request)
-      .toPromise()
-      .then(async (data: PayloadDownload) => {
-        await this.downloadFile(data, 'rctc.xlsx')
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  }
-
-  async downloadJsonBundle() {
-    this.httpClient
-      .post('/api/download/jsonbundle', this.request)
-      .toPromise()
-      .then(async (data: PayloadDownload) => {
-        await this.downloadFile(data, 'bundle.json')
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  }
-
-  async downloadXmlBundle() {
-    this.httpClient
-      .post('/api/download/xmlbundle', this.request)
-      .toPromise()
-      .then(async (data: PayloadDownload) => {
-        await this.downloadFile(data, 'bundle.xml')
-      })
-      .catch(err => {
-        console.log(err);
-      });
+  async downloadFile(data, filename?) {
+    let blob: Blob;
+    if (this.contentType === 'json') {
+      blob = new Blob([JSON.stringify(data, null, 2)],{ type: 'application/json;charset=utf-8' })
+    } else if (this.contentType === 'xml') {
+      blob = new Blob([data],{ type: 'text/xml' })
+    }
+    saveAs(blob, filename);
   }
 }
